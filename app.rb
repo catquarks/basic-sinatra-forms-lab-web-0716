6ac14f44c89f382b8f209677ff34fc7095506e0a
Tami <catquarks@gmail.com>
2016-08-12 20:21:38 -0400

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+	get '/newteam' do 
+		erb :newteam
+	end
+
+	post '/newteam' do 
+		@keys = %w[name coach pg sg pf sf c]
+		@team_info = @keys.each_with_object({}) {|item, hash| hash[item] = params[item]}
+		
+		erb :team
+	end
+
+
 
 end
